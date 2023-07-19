3d6b3f045928fe924de7e1e497d98be78e05f544
smDanisaMinchola <106545006+smDanisaMinchola@users.noreply.github.com>
2023-07-19 17:05:40 -0500

    Add files via upload

---
+++ porta/script.js
@@ -0,0 +1,107 @@
+let nav_img_x = document.querySelector('.x');
+let nav_img_bars = document.querySelector('.bars');
+let navbar = document.querySelector('.list');
+let body=document.querySelector('body');
+
+nav_img_bars.onclick = function() {
+    navbar.classList.add('mostrar');
+    nav_img_bars.classList.add("desaparecer");
+}
+
+nav_img_x.onclick = function() {
+    navbar.classList.remove('mostrar');
+    nav_img_bars.classList.remove("desaparecer");
+
+}
+
+// ----------------------------------------------------------------------------------------------------------------------
+let bNum   = 2;     // Number of bubbles created on movement (2)
+let bSize  = 8;     // Bubble size (8)
+let bSpeed = 3;     // Bubble speed (3)
+let bDep   = 0.01;  // Bubble depletion speed (0.01)
+let bDist  = 40;    // Mucus length (40)
+
+/* --- Main Program: DO NOT EDIT BELOW --- */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let spots = [];
+let hue = 0;
+
+const mouse = {
+	x: undefined,
+	y: undefined
+}
+
+canvas.addEventListener("mousemove", function(event){
+	mouse.x = event.x;
+	mouse.y = event.y;
+	
+	for (let i = 0; i < bNum; i++) {
+		spots.push(new Particle());
+	}
+})
+
+window.addEventListener("resize", function() {
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+	init();
+})
+
+class Particle {
+	constructor() {
+		this.x = mouse.x;
+		this.y = mouse.y;
+		this.size = Math.random() * bSize + 0.1;
+		this.speedX = Math.random() * bSpeed - bSpeed/2;
+		this.speedY = Math.random() * bSpeed - bSpeed/2;
+		this.color = "hsl(" + hue + ", 100%, 50%)";
+	}
+	update() {
+		this.x += this.speedX;
+		this.y += this.speedY;
+		if (this.size > bDep) this.size -= bDep;
+	}
+	draw() {
+		ctx.fillStyle = this.color;
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+		ctx.fill();
+	}
+}
+
+function handleParticle() {
+	for (let i = 0; i < spots.length; i++) {
+		spots[i].update();
+		spots[i].draw();
+		for (let j = i; j < spots.length; j++) {
+			const dx = spots[i].x - spots[j].x;
+			const dy = spots[i].y - spots[j].y;
+			const distance = Math.sqrt(dx * dx + dy * dy);
+			if (distance < bDist) {
+				ctx.beginPath();
+				ctx.strokeStyle = spots[i].color;
+				ctx.lineWidth = spots[i].size / 3;
+				ctx.moveTo(spots[i].x, spots[i].y);
+        ctx.lineTo(spots[j].x, spots[j].y);
+				//ctx.bezierCurveTo(spots[j].x, spots[j].y, spots[j].x, spots[i].y, spots[j].x, spots[j].y);
+				ctx.stroke();
+			}
+		}
+		if (spots[i].size <= bDep) {
+			spots.splice(i, 1); 
+			i--;
+		}
+	}
+}
+
+function animate() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	handleParticle();
+	hue++;
+	requestAnimationFrame(animate);
+}
+
+animate()
